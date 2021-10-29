3ed0c3cc72f3ed2ef4b471a32d55bddfd54d4835
1996zrq <598646798@qq.com>
2021-10-29 20:55:35 +0800

    blood_over

--- patients/models.py
+++ patients/models.py
@@ -174,7 +174,9 @@ class BPatientRtms(models.Model):
 class RPatientBlood(models.Model):
     patient_session = models.ForeignKey(DPatientDetail, models.DO_NOTHING)
     blood_sample_id = models.CharField(max_length=45, blank=True, null=True)
+    fake_name = models.CharField(max_length=45, blank=True, null=True)
     blood_sampling_date = models.DateField(blank=True, null=True)
+    centrifugal_date = models.DateField(blank=True, null=True)
     inspect_date = models.DateField(blank=True, null=True)
     total_blood_number = models.IntegerField(blank=True, null=True)
     plasma_number = models.IntegerField(blank=True, null=True)
