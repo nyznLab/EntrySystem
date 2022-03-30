35335c1fe1986e94a9361fa18e30b24df4a7dacf
suzhao <suzhao10086@gmail.com>
2022-03-30 20:24:39 +0800

    change database

--- EntrySystem/settings.py
+++ EntrySystem/settings.py
@@ -91,10 +91,10 @@ WSGI_APPLICATION = 'EntrySystem.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',  # 数据库引擎
-        'NAME': 'entrySystem',  # 你要存储数据的库名，事先要创建之
-        'USER': 'root',  # 数据库用户名
-        'PASSWORD': 'EntrySystem123456:',  # 密码
-        'HOST': '192.168.0.201',  # 主机
+        'NAME': 'entry_system',  # 你要存储数据的库名，事先要创建之
+        'USER': 'developer',  # 数据库用户名
+        'PASSWORD': 'develop_123!123',  # 密码
+        'HOST': '10.0.0.75',  # 主机
         'PORT': '3306',  # 数据库使用的端口
     }
 }
